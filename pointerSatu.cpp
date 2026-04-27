9ad38e537f28ad21a516af58e3402331cb11ae4a
nababanalmaidah <nababanalmaidah@gmail.com>
2026-04-27 17:42:39 +0700

    first commit

---
+++ pointerSatu.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout<<"NO Induk = "<<nim<<endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{1}; // object mhs
+    mhs.showNim
+}

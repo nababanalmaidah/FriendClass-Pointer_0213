47efa43ec52cc42eb6d9dde8d950d7da1decd6c5
nababanalmaidah <nababanalmaidah@gmail.com>
2026-04-27 18:22:48 +0700

    commit6

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -12,3 +12,20 @@ public:
     void setNama(string pNama);
     friend class siswa;
 };
+
+class siswa
+{
+private:
+    int id;
+
+public:
+    void setId(int pId);
+    void displayAll(orang &a);
+};
+
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
+void orang::setNama(string pNama)
+{}
